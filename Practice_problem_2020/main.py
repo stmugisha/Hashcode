508fe9333f5bc85ca33def5f20a81b36e186c067
steph-en-m <mstevejean@gmail.com>
2020-01-23 16:54:57 +0300

    Source code

---
+++ Practice_problem_2020/main.py
@@ -0,0 +1,55 @@
+# M: max. number of pizza slices
+# N: num of different pizza types
+# S: num of slices in each pizza type (S[n-1] <= M)
+import numpy as np
+
+
+def read(filename: str) -> (int,int,list):
+    """Read an input file"""
+    with open(filename, 'r') as file:
+        M, N = map(int, file.readline().rstrip('\n').split())
+        S = [list(map(int, line.rstrip('\n').split())) for line in file]
+        S = S[0]
+        t = [i for i in range(len(S))]
+        S = dict(list(zip(t, S)))
+    return (M, N, S)
+
+
+def orders(d: dict, N: int, M: int) -> list:
+    """Computes and return a slice(sub-array) of `d`
+    whose elements have a combined sum less than or equal
+    to `M`.
+    """
+    current_sum = d[0]
+    pizza_list = []
+    start = 0
+
+    for i in d.keys():
+        current_sum += d[i]
+        if(current_sum > M):
+            current_sum -= d[start]
+            start += 1
+        else:
+            pizza_list.append(i)   
+    return pizza_list
+
+def write(outputfile: str, typelist: list):
+    """Writes the results of `typelist` to a text file.
+    Args:
+    `outputfile`: The name of the file to write to.
+
+    `typelist`  : A list of pizza types to order.
+    """
+    with open(outputfile, 'w') as out:
+        type_count = len(typelist)
+        out.write(''.join([str(type_count), '\n']))
+        for t in typelist:
+            out.write(''.join([str(t), ' ']))
+
+
+if __name__=='__main__':
+    M, N, S = read('./in/e_also_big.in')
+    print(f'M:{M}\nN:{N}\nS:{S}')
+    t_list = orders(S, N, M)
+    print(f'Orders: {t_list}')
+    write('./out/big.out', t_list)
